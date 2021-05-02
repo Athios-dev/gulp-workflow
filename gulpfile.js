8aba3902b9806eb7323172b09bb9bf0e93cc79e6
Thibaud d'Arros <thibaud.arros@gmail.com>
2021-05-02 16:55:34 +0200

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -13,7 +13,7 @@ var cleanCSS = require('gulp-clean-css');
 // https://github.com/terinjokes/gulp-uglify/
 var uglify = require('gulp-uglify');
 
-//  PATHS
+// 📁 PATHS
 var paths = {
     inputs: {
         css: './src/scss/**/*.scss',
